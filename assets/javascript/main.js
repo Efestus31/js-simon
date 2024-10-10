37e9e4fd3a3c36b8e31a0c5a73e5f10edd92cafb
Efestus31 <biagiohd@gmail.com>
2024-10-10 16:39:10 +0200

    add timer js add style to css add boostrap to html

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,7 +5,19 @@ for(let i = 0; i < 5; i++);{
     numeri.push(Math.floor(Math.random() * 100));
 }
 
-let numSelected = document.querySelectorAll('.num');
+//mostriamo i numeri nella pagina
+const numSelected = document.querySelectorAll('.num');
+numSelected.forEach((element, index) => {
+    element.textContent = numeri[index];
+});
+
+//timer di 30 sec
+setTimeout(() => {
+// Nascondi i numeri
+    numSelected.forEach(element => element.style.display = 'none');
+     // Mostra il form
+    document.getElementById('form').style.display = 'block'; 
+}, 30000);
 
 
 
