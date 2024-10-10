3a9ffafb91fbfe1561bf9bc3242ba0f14b036019
Efestus31 <biagiohd@gmail.com>
2024-10-10 15:53:46 +0200

    add numeri casuali js add form html

---
+++ assets/javascript/main.js
@@ -0,0 +1,11 @@
+
+//numeri casuali
+let numeri = [];
+for(let i = 0; i < 5; i++);{
+    numeri.push(Math.floor(Math.random() * 100));
+}
+
+let numSelected = document.querySelectorAll('.num');
+
+
+
